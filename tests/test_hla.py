b15cb0581b13e7d16713081a052cca7b0d059638
Popp <georg.popp@izi.fraunhofer.de>
2024-11-21 14:15:10 +0100

    added multiple tests for valid and invalid allele strings

--- tests/test_hla.py
+++ tests/test_hla.py
@@ -2,8 +2,9 @@ import unittest
 from py_hla_match.exceptions import MalformedHLAStringError
 from py_hla_match import HLA
 
+
 class TestHLA(unittest.TestCase):
-    
+
     def test_valid_hla_with_suffix(self):
         hla = HLA("HLA-A*32:11Q")
         self.assertEqual(hla.locus, "A")
@@ -40,10 +41,117 @@ class TestHLA(unittest.TestCase):
         self.assertIsNone(hla.non_coding_variant)
         self.assertIsNone(hla.suffix)
 
+    def test_valid_hla_with_non_coding_variant(self):
+        hla = HLA("HLA-DRB1*13:01:01:02")
+        self.assertEqual(hla.locus, "DRB1")
+        self.assertEqual(hla.allele_group, "13")
+        self.assertEqual(hla.allele, "01")
+        self.assertEqual(hla.synonymous_variant, "01")
+        self.assertEqual(hla.non_coding_variant, "02")
+        self.assertIsNone(hla.suffix)
+
+    def test_valid_hla_with_null_suffix(self):
+        hla = HLA("HLA-A*24:09N")
+        self.assertEqual(hla.locus, "A")
+        self.assertEqual(hla.allele_group, "24")
+        self.assertEqual(hla.allele, "09")
+        self.assertIsNone(hla.synonymous_variant)
+        self.assertIsNone(hla.non_coding_variant)
+        self.assertEqual(hla.suffix, "N")
+
+    def test_valid_hla_with_low_expression_suffix(self):
+        hla = HLA("HLA-A*30:14L")
+        self.assertEqual(hla.locus, "A")
+        self.assertEqual(hla.allele_group, "30")
+        self.assertEqual(hla.allele, "14")
+        self.assertIsNone(hla.synonymous_variant)
+        self.assertIsNone(hla.non_coding_variant)
+        self.assertEqual(hla.suffix, "L")
+
+    def test_valid_hla_with_secreted_suffix(self):
+        hla = HLA("HLA-B*44:02:01:02S")
+        self.assertEqual(hla.locus, "B")
+        self.assertEqual(hla.allele_group, "44")
+        self.assertEqual(hla.allele, "02")
+        self.assertEqual(hla.synonymous_variant, "01")
+        self.assertEqual(hla.non_coding_variant, "02")
+        self.assertEqual(hla.suffix, "S")
+
+    def test_valid_hla_without_prefix(self):
+        hla = HLA("A*01:01:01")
+        self.assertEqual(hla.locus, "A")
+        self.assertEqual(hla.allele_group, "01")
+        self.assertEqual(hla.allele, "01")
+        self.assertEqual(hla.synonymous_variant, "01")
+        self.assertIsNone(hla.non_coding_variant)
+        self.assertIsNone(hla.suffix)
+        self.assertIsNone(hla.group_code)
+
+    def test_valid_hla_with_group_code_G(self):
+        hla = HLA("DQB1*06:02:01G")
+        self.assertEqual(hla.locus, "DQB1")
+        self.assertEqual(hla.allele_group, "06")
+        self.assertEqual(hla.allele, "02")
+        self.assertEqual(hla.synonymous_variant, "01")
+        self.assertIsNone(hla.non_coding_variant)
+        self.assertIsNone(hla.suffix)
+        self.assertEqual(hla.group_code, "G")
+
+    def test_valid_hla_with_group_code_P(self):
+        hla = HLA("DQA1*05:88P")
+        self.assertEqual(hla.locus, "DQA1")
+        self.assertEqual(hla.allele_group, "05")
+        self.assertEqual(hla.allele, "88")
+        self.assertIsNone(hla.synonymous_variant)
+        self.assertIsNone(hla.non_coding_variant)
+        self.assertIsNone(hla.suffix)
+        self.assertEqual(hla.group_code, "P")
+
+    def test_valid_hla_with_three_digit_fields(self):
+        hla = HLA("A*68:114:02")
+        self.assertEqual(hla.locus, "A")
+        self.assertEqual(hla.allele_group, "68")
+        self.assertEqual(hla.allele, "114")
+        self.assertEqual(hla.synonymous_variant, "02")
+        self.assertIsNone(hla.non_coding_variant)
+        self.assertIsNone(hla.suffix)
+        self.assertIsNone(hla.group_code)
+
+    def test_valid_hla_with_full_fields(self):
+        hla = HLA("C*06:127:01:02")
+        self.assertEqual(hla.locus, "C")
+        self.assertEqual(hla.allele_group, "06")
+        self.assertEqual(hla.allele, "127")
+        self.assertEqual(hla.synonymous_variant, "01")
+        self.assertEqual(hla.non_coding_variant, "02")
+        self.assertIsNone(hla.suffix)
+        self.assertIsNone(hla.group_code)
+
+    def test_invalid_suffix(self):
+        with self.assertRaises(MalformedHLAStringError):
+            HLA("HLA-A*32:11X")
+
+    def test_invalid_suffix_numeric(self):
+        with self.assertRaises(MalformedHLAStringError):
+            HLA("HLA-A*24:02:01:01:01")
+
+    def test_invalid_suffix_special_character(self):
+        with self.assertRaises(MalformedHLAStringError):
+            HLA("HLA-B*44:02@")
+
+    def test_invalid_suffix_lowercase(self):
+        with self.assertRaises(MalformedHLAStringError):
+            HLA("HLA-A*24:02l")
+
+    def test_invalid_hla_with_incorrect_field(self):
+        with self.assertRaises(MalformedHLAStringError):
+            HLA("HLA-A*02:01X:01")
+
     def test_repr(self):
         hla = HLA("HLA-A*32:11Q")
         expected_repr = (
             "HLA(allele_string='HLA-A*32:11Q', locus='A', allele_group='32', "
-            "allele='11', synonymous_variant=None, non_coding_variant=None, suffix='Q')"
+            "allele='11', synonymous_variant=None, non_coding_variant=None, "
+            "suffix='Q', group_code=None)"
         )
         self.assertEqual(repr(hla), expected_repr)
