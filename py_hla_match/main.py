1b71b8d7463867fd5762a0cda0d8c3518a4d2697
Tim Adams <tim.adams@scai.fraunhofer.de>
2024-11-21 10:37:47 +0100

    chore: remove placeholder classes

--- py_hla_match/main.py
+++
@@ -1,6 +0,0 @@
-# main.py -> remove as soon as we have actual logic
-def hla_match_example(a, b):
-    """
-    Example function to test HLA match.
-    """
-    return a == b
--- tests/test_main.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from py_hla_match.main import hla_match_example
-
-def test_hla_match_example():
-    assert hla_match_example(1, 1) is True
-    assert hla_match_example(1, 2) is False
