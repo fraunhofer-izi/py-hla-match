f2b84af3d935ee3f88dbcf2e54f80ee9a47a864d
Tim Adams <tim.adams@scai.fraunhofer.de>
2025-02-12 11:37:44 +0100

    Feat: Add Exception for parsing error

--- py_hla_match/exceptions.py
+++ py_hla_match/exceptions.py
@@ -33,3 +33,22 @@ class InvalidLocusComparisonError(Exception):
 
     def __str__(self):
         return super().__str__()
+
+
+class MalformedHLADataSourceError(Exception):
+    """
+    Error raised when an HLA data source is malformed or cannot be parsed.
+    """
+    def __init__(self, message, details=None):
+        """
+        :param message: Error message.
+        :param details: Optional additional details about the error.
+        """
+        super().__init__(message)
+        self.details = details
+
+    def __str__(self):
+        base_message = super().__str__()
+        if self.details:
+            return f"{base_message} (Details: {self.details})"
+        return base_message
